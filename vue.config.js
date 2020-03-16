76c982b9e5bdc78aa1ff51f3c2efa499aea9cf1b
wakawaka666 <eason1126500186@gmail.com>
2020-03-17 01:42:39 +0800

    路径

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   // 基本路径
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/home/Yang/apps/Vue-admin/dist' : '/home/Yang/apps/Vue-admin/dist',
   // 输出文件目录
   outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
   // eslint-loader 是否在保存的时候检查
